ef1127bab5cd63279fa4a473b73ed42c45b36f13
Matt Tran <MattTran6@gmail.com>
2016-07-18 11:47:30 -0600

    finished project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 angular.module('chatroom', []).config(function($httpProvider){
-  
+
 });
+
+// app.filter('reverse',function(){
+//   return function(items){
+//     return items.slice().reverse();
+//   };
+// });
